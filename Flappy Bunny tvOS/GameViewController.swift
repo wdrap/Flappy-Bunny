43d7c232254ce3c92a9fe9b97ac54964b2274a7d
wdrap <wdrap@com.github.local>
2021-01-09 10:49:28 +0100

    Initial Commit

---
+++ Flappy Bunny tvOS/GameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameViewController.swift
+//  Flappy Bunny tvOS
+//
+//  Created by Wim Drapier on 09/01/2021.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+        
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+}
